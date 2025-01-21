b66746e06c56c1cc651e4f47ed36164d18ca6a1f
Muhammad Iqbal <muhammadiqbalid43@gmail.com>
2025-01-21 09:23:35 +0700

    feat: dialog text

---
+++ src/constants/dialog.ts
@@ -0,0 +1,8 @@
+const dialogues = [
+  "Welcome to the world of Pokémon!",
+  "My name is Oak. People affectionately refer to me as the Pokémon Professor.",
+  "Before we go any further, I'd like to tell you a few things you should know about this world!",
+  "This world is inhabited far and wide by creatures called Pokémon.",
+  "For some people, Pokémon are pets. Others use them for battling.",
+  "As for myself, I study Pokémon as a profession.",
+];
