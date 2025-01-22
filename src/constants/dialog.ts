81ca3405a01e0cb68a43a94bfdaf42310ae9ecc9
Muhammad Iqbal <muhammadiqbalid43@gmail.com>
2025-01-22 13:49:12 +0700

    chore: install react-icon

--- src/constants/dialog.ts
+++ src/constants/dialog.ts
@@ -1,8 +1,22 @@
-const dialogues = [
+export const dialogues = [
   "Welcome to the world of Pokémon!",
   "My name is Oak. People affectionately refer to me as the Pokémon Professor.",
   "Before we go any further, I'd like to tell you a few things you should know about this world!",
   "This world is inhabited far and wide by creatures called Pokémon.",
   "For some people, Pokémon are pets. Others use them for battling.",
   "As for myself, I study Pokémon as a profession.",
+  "But first, tell me a little bit about yourself.",
+  "Such a beautiful name!",
+  "Your very own tale of grand adventure is about to unfold.",
+  "Fun experiences, difficult experiences, there's so much waiting for you!",
+  "Dreams! Adventure!",
+  "Let's go to the world of Pokémon! I'll see you later!",
+];
+
+export const dialogueText = [
+  "Welcome to the world of Pokémon!",
+  "My name is Oak. People affectionately refer to me as the Pokémon Professor.",
+  "This world is inhabited far and wide by creatures called Pokémon!",
+  "For some people, Pokémon are pets. Others use them for battling.",
+  "As for myself... I study Pokémon as a profession.",
 ];
